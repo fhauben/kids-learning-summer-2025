e0dd6d304f09421c8e2f2e3363e3e89c8753727d
Frank Hauben <frank@coderpad.io>
2025-06-29 08:52:26 -0700

    Remove nested project to clean up repository structure

--- learning-prototype/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    darkMode: 'class', // enable dark mode via a CSS class
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
